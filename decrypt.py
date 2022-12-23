8542600d2fbda85fac806e42d737f733eceaa7fd
Stephan Skanta <78694630+techcree@users.noreply.github.com>
2022-12-23 15:10:17 +0100

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,30 +1,42 @@
-#ssk TechCree 2022
+#ssk TechCree2022
 def encrypt(message, key):
-    encrypted_message = input("Entschlüsseln von: ")
-    
-    for char in message:
+  # Create an empty string to store the encrypted message
+  encrypted_message = input("Entschlüsseln von: ")
+
+  # Loop through each character in the message
+  for char in message:
+    # Get the ASCII value of the character
     ascii_val = ord(char)
 
+    # Encrypt the character by adding the key to the ASCII value
     encrypted_char = chr(ascii_val + key)
 
+    # Add the encrypted character to the encrypted message
     encrypted_message += encrypted_char
 
   return encrypted_message
 
 def decrypt(encrypted_message, key):
-    message = ""
+  # Create an empty string to store the decrypted message
+  message = ""
 
-    for char in encrypted_message:
-        ascii_val = ord(char)
+  # Loop through each character in the encrypted message
+  for char in encrypted_message:
+    # Get the ASCII value of the character
+    ascii_val = ord(char)
 
+    # Decrypt the character by subtracting the key from the ASCII value
     decrypted_char = chr(ascii_val - key)
 
+    # Add the decrypted character to the message
     message += decrypted_char
 
   return message
 
+# Test the cipher
 message = ""
 key = 3
 encrypted_message = encrypt(message, key)
+#print(encrypted_message)
 decrypted_message = decrypt(encrypted_message, key)
 print(decrypted_message)
